55e276e4258c2a0dfa34f43a83bf4286a7c9ef65
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-01-25 22:19:57 +0900

    profile ++, interests

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,49 +1,69 @@
 class ProfilesController < ApplicationController
-  before_filter :create_profile_if_not, :showing_profile_of_id
+  before_filter :create_profile_if_not, :visiting_user_id
   before_filter :is_this_user_profile, only: [:edit]
+  attr_accessor :profiles, :out_visitors, :in_visitors, :out_visitors,
+  :in_interests, :out_interests, :is_owner, :current_user_profile, :visiting_user_id
 
   # GET /profiles
   def index
     @profiles = Profile.all
-    @my_visitors = @current_user_profile.visitors
-    @my_interests = current_user.interests
+    @out_visitors = current_user.visitors
+    @in_visitors = Visitor.where(viewed_id: current_user.id)
+
+    @in_interests = Interest.where(to_user_id: current_user.id)
+    @out_interests_responses = Interest.where(user_id: current_user.id)
   end
 
   # GET /profiles/1
   def show
-    find_first = current_user.profiles.first
-    if !@is_owner
-      if !@his_profile.visitors.first
-        @his_profile.visitors.create(viewer_id: find_first.id)
+    if current_user.id != @visiting_user_id.to_i
+      find_first = current_user.visitors.where(viewed_id: @visiting_user_id).first
+      if !find_first
+        current_user.visitors.create(viewed_id: @visiting_user_id)
       else
         find_first.touch
       end
     end
   end
 
-  # GET /profiles/new
-  def new
-    @profile = Profile.new
-  end
-
+  # After the edit is pressed
   def modify
     current_user.profiles.first.update(:sex => params['sex'])
-    # To do change to /profile/:this id/path
     redirect_to(profiles_path)
   end
 
+  # Delete this Profile Button
   def remove
-    current_user.profiles.first.destroy
+    current_user.destroy
     redirect_to(profiles_path)
   end
 
+  # Express Interest Button
   def interest
-    find_first = current_user.interests.first
-    if !find_first
-      current_user.interests.create(to_user_id: params[:to_user_id])
-    else
-      find_first.touch
+    if current_user.id != params[:to_user_id].to_i
+      find_first = current_user.interests.where(to_user_id: params[:to_user_id]).first
+      logger.info("Debug Params inspect #{params.inspect}")
+      if !find_first
+        current_user.interests.create(to_user_id: params[:to_user_id])
+      else
+        find_first.touch
+      end
     end
+    redirect_to(profiles_path)
+  end
+
+  # When accept or reject button is clicked
+  def interest_response
+    commit = params[:commit]
+
+    interest = User.find(params[:from_user_id]).interests.where(:to_user_id => current_user.id).first
+
+    if commit == "Accept"
+      interest.update(:response => 1)
+    else
+      interest.update(:response => 0)
+    end
+
     redirect_to(profiles_path)
   end
 
@@ -52,22 +72,17 @@ class ProfilesController < ApplicationController
     if !current_user.profiles.first
       current_user.profiles.create()
     end
-
     @current_user_profile = @current_user.profiles.first
-    if (@current_user_profile.id == params[:id].to_i)
-      @is_owner = true
-    else
-      @is_owner = false
-    end
+    @is_owner = (@current_user_profile.id == params[:id].to_i) ? true : false
   end
 
-  def showing_profile_of_id
+  def visiting_user_id
     if params[:id]
-      @showing_profile_of_id = params[:id]
-      @his_profile = Profile.find(params[:id])
+      @visiting_user_id = Profile.find(params[:id]).user_id
     end
   end
 
+  # for editing profile
   def is_this_user_profile
     if (@current_user_profile.id != params[:id].to_i)
       redirect_to(profiles_path)
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,3 @@
 class Profile < ActiveRecord::Base
   belongs_to :users
-  has_many :visitors
-  has_many :interests
 end
