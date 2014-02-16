ac05f5c39c7c9e2bc86e6db531c86c7812eb0c38
Pulkit Kathuria <kevincobain2000@gmail.com>
2014-02-16 20:45:36 +0900

    sex to permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   # protect_from_forgery with: :null_session
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :sex
+  end
 end
