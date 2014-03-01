37de94ead5d456dd82b012869f44799938b659dd
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-03-01 09:41:09 +0900

    migrationg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+//= require jquery.ui.all
 //= require parsley
 //= require bootstrap-datepicker
 //= require bootstrap-timepicker
