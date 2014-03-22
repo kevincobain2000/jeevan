b6119f0c8205a1369f2970db25e7ecd27f9bd525
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-03-22 11:54:58 +0900

    Search is hooked with datatables filtering search bar is custom and uses oTable.fnFilter See here for search: http://www.datatables.net/ref#fnFilter

--- app/assets/javascripts/explore.js.coffee
+++ app/assets/javascripts/explore.js.coffee
@@ -1,4 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).on "page:change", ->
+  if (typeof gon != 'undefined' && gon.search)
+
+    oTable = $("#search").dataTable
+      aaData:gon.search['profiles']
+      bFilter: true
+      bLengthChange:false
+
+    $(".dataTables_filter input").hide()
+    $(".dataTables_filter").children("label").hide()
+    $("#search-query").keyup ->
+      oTable.fnFilter $("#search-query").val()
--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -12,16 +12,28 @@ class ExploreController < ApplicationController
   end
 
   def search
-    @searched_profiles = Hash.new {|h, k| h[k] = [] }
-    # Todo Take interests donot add profiles to show to whom interests have been sent
-    # users_not_my_gender = User.where.not(sex: current_user.sex, :id.in(@interests[:in])).order('created_at DESC').limit(1000)
-    users = User.where.not(sex: current_user.sex).order('created_at DESC').limit(1000)
-    @profiles_paginate = @searched_profiles.keys.paginate(:page => params[:page], :per_page => 4)
-    users.each do |user|
-      @searched_profiles[user.id] = make_user(user)
-    end
+    gon.search = {}
+    gon.search['profiles']  = make_gon_search(Interest.find(:all))
     render 'search'
   end
+
+  def make_gon_search(obj)
+    ret = []
+    obj.each do |i|
+      user = User.find(i.user_id)
+
+      avatar = "<img src='#{user.avatar}' class='img-rounded' style='height:50px;width:50px' >"
+      profile_link = "<a href='profiles/#{user.profile.id}'>#{user.name}</a>"
+      dob = user[:dob].gsub("/","-")
+      age = time_ago_in_words(Date::strptime(dob, "%m-%d-%Y"), Time.now)
+
+      updated_at = "#{time_ago_in_words(user.updated_at)} ago"
+
+      ret << [avatar, profile_link, user.religion.religion, user.kundali.birth_city, user.profile.posted_by, age, updated_at]
+    end
+    return ret
+  end
+
   def show
     if params[:id] == 'search'
       search
