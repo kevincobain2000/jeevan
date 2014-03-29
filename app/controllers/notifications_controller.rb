ee4970c20c32f6f9a24083d2e241f94fd3bee89e
Pulkit Kathuria <kevincobain2000@gmail.com>
2014-03-29 12:36:45 +0900

    removed it

--- app/controllers/notifications_controller.rb
+++
@@ -1,7 +0,0 @@
-class NotificationsController < ApplicationController
-  def seen
-    # todo
-    # Interest.where("to_user_id = ? OR user_id = ? AND seen <> ?", current_user.id, current_user.id, 2).update_all(:seen => 2)
-    render json: { :status => 200 }
-  end
-end
