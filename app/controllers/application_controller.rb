b3b431763977d9068a88b90d43fb8a059741a004
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-04-05 12:27:09 +0900

    visuals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,11 @@ class ApplicationController < ActionController::Base
 
   def make_user(user)
     dob = user.dob.gsub("/","-")
-    age = user.dob.empty? ? nil: distance_of_time_in_words(Date::strptime(dob, "%m-%d-%Y"), Time.now)
+    age = calculate_age(dob)
     user_ret = {
       id:         user.id,
       dob:        user.dob,
-      age:        age.titleize,
+      age:        age,
       name:       titleize(truncate(user.name)),
       updated_at: time_ago_in_words(user.updated_at),
       sex:        titleize(user.sex),
@@ -39,6 +39,9 @@ class ApplicationController < ActionController::Base
       shortlist:    Shortlist.where(user_id: current_user.id, to_user_id: user.id).first,
     }
     return user_ret;
+  end
+  def calculate_age(birthday)
+    Date.today.year - birthday.to_date.year
   end
   protected
   def configure_permitted_parameters
@@ -88,10 +91,3 @@ class ApplicationController < ActionController::Base
     end
   end
 end
-
-
-
-
-
-
-
