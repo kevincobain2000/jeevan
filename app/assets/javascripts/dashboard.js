a656d8c0967c8fe992d476c611b01de5f029f576
Pulkit Kathuria <kevincobain2000@gmail.com>
2014-04-05 09:55:34 +0900

    migrated to js

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,51 +1,49 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).on "page:change", ->
-  $('#tabs').tabs()
 
-  try
-    $("#in-interests").dataTable().fnDestroy()
-    $("#out-interests").dataTable().fnDestroy()
-    $("#in-visitors").dataTable().fnDestroy()
-    $("#shortlist").dataTable().fnDestroy()
-  catch e
-    # ...
+$(document).on("page:change", function() {
+  var in_interests, in_visitors, out_interests, shortlist;
+  $('#tabs').tabs();
+  try {
+    $("#in-interests").dataTable().fnDestroy();
+    $("#out-interests").dataTable().fnDestroy();
+    $("#in-visitors").dataTable().fnDestroy();
+    $("#shortlist").dataTable().fnDestroy();
+  } catch (e) {
 
-
-  if (typeof gon != 'undefined' && gon.dashboard)
-
-    in_interests = $("#in-interests").dataTable
-      aaData:gon.dashboard['in_interests']
-      bFilter: true
-      bLengthChange:false
-    $("#search-in-interests").keyup ->
-      in_interests.fnFilter $(this).val()
-
-
-    out_interests = $("#out-interests").dataTable
-      aaData:gon.dashboard['out_interests']
-      bFilter: true
-      bLengthChange:false
-    $("#search-out-interests").keyup ->
-      out_interests.fnFilter $(this).val()
-
-    in_visitors = $("#in-visitors").dataTable
-      aaData:gon.dashboard['in_visitors']
-      bFilter: true
-      bLengthChange:false
-    $("#search-in-visitors").keyup ->
-      in_visitors.fnFilter $(this).val()
-
-    shortlist = $("#shortlist").dataTable
-      aaData:gon.dashboard['shortlist']
-      bFilter: true
-      bLengthChange:false
-    $("#search-shortlist").keyup ->
-      in_visitors.fnFilter $(this).val()
-
-    $(".dataTables_filter input").hide()
-    $(".dataTables_filter").children("label").hide()
-
-
-
+  }
+  if (typeof gon !== 'undefined' && gon.dashboard) {
+    in_interests = $("#in-interests").dataTable({
+      aaData: gon.dashboard['in_interests'],
+      bFilter: true,
+      bLengthChange: false
+    });
+    $("#search-in-interests").keyup(function() {
+      return in_interests.fnFilter($(this).val());
+    });
+    out_interests = $("#out-interests").dataTable({
+      aaData: gon.dashboard['out_interests'],
+      bFilter: true,
+      bLengthChange: false
+    });
+    $("#search-out-interests").keyup(function() {
+      return out_interests.fnFilter($(this).val());
+    });
+    in_visitors = $("#in-visitors").dataTable({
+      aaData: gon.dashboard['in_visitors'],
+      bFilter: true,
+      bLengthChange: false
+    });
+    $("#search-in-visitors").keyup(function() {
+      return in_visitors.fnFilter($(this).val());
+    });
+    shortlist = $("#shortlist").dataTable({
+      aaData: gon.dashboard['shortlist'],
+      bFilter: true,
+      bLengthChange: false
+    });
+    $("#search-shortlist").keyup(function() {
+      return in_visitors.fnFilter($(this).val());
+    });
+    $(".dataTables_filter input").hide();
+    return $(".dataTables_filter").children("label").hide();
+  }
+});
--- app/assets/javascripts/explore.js
+++ app/assets/javascripts/explore.js
@@ -1,18 +1,23 @@
-$(document).on "page:change", ->
 
-  try
-    $("#search").dataTable().fnDestroy()
-  catch e
+$(document).on("page:change", function() {
+  var oTable;
+  try {
+    $("#search").dataTable().fnDestroy();
+  } catch (e) {
 
-  if (typeof gon != 'undefined' && gon.search)
+  }
+  if (typeof gon !== 'undefined' && gon.search) {
     console.log(gon.search);
-    oTable = $("#search").dataTable
-      aaData:gon.search['profiles']
-      bFilter: true
-      bLengthChange:false
+    oTable = $("#search").dataTable({
+      aaData: gon.search['profiles'],
+      bFilter: true,
+      bLengthChange: false,
       iDisplayLength: 30
-
-    $(".dataTables_filter input").hide()
-    $(".dataTables_filter").children("label").hide()
-    $("#search-query").keyup ->
-      oTable.fnFilter $("#search-query").val()
+    });
+    $(".dataTables_filter input").hide();
+    $(".dataTables_filter").children("label").hide();
+    return $("#search-query").keyup(function() {
+      return oTable.fnFilter($("#search-query").val());
+    });
+  }
+});
