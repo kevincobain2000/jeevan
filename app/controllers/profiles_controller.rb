6ae55ad77c89a0064e1b5996d63dfdc8e87f0ab5
Pulkit Kathuria <kevincobain2000@gmail.com>
2014-04-26 00:29:32 +0900

    chhuhai

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -159,6 +159,22 @@ class ProfilesController < ApplicationController
   end
   #-----  End of Ajax Calls  ------#
 
+  def index
+    # Currently the search results are shown as datatables. The other way is to use will_paginate. 
+    # If want to do will_paginate way then uncomment the following Matches and comment the bottom search
+    # Experiment if the datatables are able to handle 10000 records
+
+    #-----  Matches  ------
+    # @profiles_matches = Hash.new {|h, k| h[k] = [] }
+    # users = User.where.not(sex: current_user.sex).where(devotion: current_user.devotion).order('avatar_updated_at DESC').limit(100)
+    # users.each do |user|
+    #   @profiles_matches[user.id] = make_user(user)
+    # end
+    # @profiles_paginate_matches = @profiles_matches.keys.paginate(:page => params[:matches], :per_page => 7)
+    @profiles_paginate_matches = User.paginate(:page => params[:page], :per_page => 10)
+    # search
+  end
+
   protected
 
   def load_gon
