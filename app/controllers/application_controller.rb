ca9e06df1827089bab433ba19a06c3000d8500f1
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-05-27 00:01:45 +0900

    improves

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -83,6 +83,14 @@ class ApplicationController < ActionController::Base
       return true
     end
   end
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :sex
