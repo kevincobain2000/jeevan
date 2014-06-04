32d71333c60fe6c97960d12308f162bdf87f548f
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-06-04 22:41:41 +0900

    autocomplete

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -312,7 +312,7 @@ $(document).on("page:change", function() {
   }
 
   $('form.edit').submit(function() {
-    $.gritter.add({ image: '/assets/warning.png', title: 'Success', text: 'Your settings have been saved' });
+    $.gritter.add({ image: '/assets/success.png', title: 'Success', text: 'Your settings have been saved' });
     // $(this).find(':submit').html('Saved');
   });
 
