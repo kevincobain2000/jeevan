4b6349589d6d29bf6e4e05137060ca16e9d13dde
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-06-07 15:38:20 +0900

    layout

--- app/controllers/others_controller.rb
+++ app/controllers/others_controller.rb
@@ -9,6 +9,9 @@ class OthersController < ApplicationController
     if params[:sex] == "Male" || params[:sex] == "Female"
       current_user.update("sex" => params[:sex])
     end
+    if params[:dob]
+      current_user.update("dob" => params[:dob])
+    end
     redirect_to root_path
   end
 end
