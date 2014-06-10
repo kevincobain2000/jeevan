3367c72eaed5e9e4e371aa9d76bf78a1bc511b1a
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-06-10 22:47:27 +0900

    linked in image is image iconnow

--- app/models/user.rb
+++ app/models/user.rb
@@ -145,7 +145,6 @@ class User < ActiveRecord::Base
     end
   end
   def self.find_for_linkedin_oauth(auth, signed_in_resource=nil)
-    logger.info("Debug #{auth.to_json}")
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if !user.nil?
       return user
